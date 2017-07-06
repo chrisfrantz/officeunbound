fd3b203de00c8941a1f0ed0e84e223723b51e470
Chris Frantz <chrisfrantz@Chriss-MacBook-Pro.local>
2017-07-05 22:39:09 -0400

    reinit

---
+++ assets/js/template.js
@@ -0,0 +1,13 @@
+/*! Load Cards into bottom of doc */
+    $(document).ready(function () {
+    $("#harry-guinness").load("partials/harry-guinness.html");
+    $("#priscilla-kim").load("partials/priscilla-kim.html");
+    $("#vicky-lai").load("partials/vicky-lai.html");
+    $("#wade-foster").load("partials/wade-foster.html");
+    $("#claudia-reynders").load("partials/claudia-reynders.html");
+    $("#katie-leigh").load("partials/katie-leigh.html");
+    $("#newsletter-footer").load("partials/newsletter-footer.html");
+    $("#newsletter-header").load("partials/newsletter-header.html");
+    $("#nav-partial").load("partials/nav-partial.html");
+    $("#modal-partial").load("partials/modal-partial.html");
+});
